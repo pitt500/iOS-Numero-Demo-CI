fdf31dd55fb3764315e2afa4044814aba0b5b80a
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-22 17:43:11 -0700

    Move ci.sh to upper folder

--- Numero/ci.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-xcodebuild -project 'Numero/Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 11' test
-xcodebuild -project 'Numero/Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
-
